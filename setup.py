791ed572668c4dbf421e1972884569d11694fb52
Jake Hansen <jhansen44@mail.csuchico.edu>
2020-08-09 00:09:08 -0700

    Add swig call to setup.py to allow easy pip install

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 setup.py file for SWIG example
 """
 
+from shutil import which
+from subprocess import run
 from distutils.core import setup, Extension
 
+# Use swig to build files required for install
+if which('swig') is None:
+    raise EnvironmentError('"swig" must be installed and present in PATH')
+run('swig -c++ -python biasedurn.i'.split())
 
+# Install module
 biasedurn_module = Extension('_biasedurn',
-                           define_macros = [#('PY_BUILD', ), TERMPORARY
-                                ('MAXCOLORS', 10000000)],
-                           sources=[
-                               'biasedurn_wrap.cxx', 'urn.cpp', 'stoc3.cpp', 
-                               'stoc2.cpp', 'stoc1.cpp', 'wnchyppr.cpp', 
-                               'fnchyppr.cpp', 
-                               'mersenne.cpp', 'mother.cpp', 'userintf.cpp'],
-                           )
-
-setup (name = 'biasedurn',
-       version = '0.1',
-       author      = "Vahan Nanumyan",
-       description = """First try to bind biased urn to python""",
-       ext_modules = [biasedurn_module],
-       py_modules = ["biasedurn"],
-       )
+                             define_macros = [('MAXCOLORS', 10000000)],
+                             sources=['biasedurn_wrap.cxx', 'urn.cpp', 'stoc3.cpp', 
+                                      'stoc2.cpp', 'stoc1.cpp', 'wnchyppr.cpp', 
+                                      'fnchyppr.cpp', 
+                                      'mersenne.cpp', 'mother.cpp', 'userintf.cpp'])
+setup(name = 'biasedurn',
+      version = '0.1',
+      author      = "Vahan Nanumyan",
+      description = """First try to bind biased urn to python""",
+      ext_modules = [biasedurn_module],
+      py_modules = ["biasedurn"])
